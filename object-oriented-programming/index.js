cc18a63cf4aed60a81c907ff7cc7b39dcccc877a
Ohran <oki.imamovic@gmail.com>
2022-12-15 19:52:14 +0100

    javascript oop course, object literals.

---
+++ object-oriented-programming/index.js
@@ -0,0 +1,18 @@
+// Cirlce Object
+let circle = {
+    
+    // Props
+    radius : 1,
+    location : {
+        x: 1,
+        y: 1
+    },
+
+    // Method
+    draw: function() {
+        console.log('The function was triggered');
+    }
+};
+
+//Calling circle object with draw method
+circle.draw();
